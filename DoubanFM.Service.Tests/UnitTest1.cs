bb6190ba587d7104e45161699cd5b050b5d43422
WangFu <wangfu91@hotmail.com>
2014-10-14 00:48:56 +0800

    Add Song Model...

---
+++ DoubanFM.Service.Tests/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+
+namespace DoubanFM.Service.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var cs1 = new Class1();
+
+            //var chanels = cs1.Get("j/app/radio/channels");
+
+            var people = cs1.Post("/j/app/radio/people");
+        }
+    }
+}
---
+++ DoubanFM.Service/Class1.cs
@@ -0,0 +1,49 @@
+﻿using RestSharp;
+
+namespace DoubanFM.Service
+{
+    public class Class1
+    {
+        public Class1()
+        {
+
+        }
+
+        RestClient client = new RestClient("http://www.douban.com/");
+
+        public string Get(string resource)
+        {
+
+            var request = new RestRequest(resource, Method.GET);
+
+            //request.AddUrlSegment("subjectID", "2272292");
+
+            //request.AddParameter("", "");
+
+            var response = client.Execute(request);
+
+            //Debug.WriteLine(response.ResponseUri);
+
+            var content = response.Content;
+
+            return content;
+        }
+
+        public string Post(string resource)
+        {
+            var request = new RestRequest(resource, Method.POST);
+
+            request.AddParameter("app_name", "radio_desktop_win");
+            request.AddParameter("version", "100");
+            request.AddParameter("sid", "");
+            request.AddParameter("channel", "1");
+            request.AddParameter("type", "n");
+
+            var response = client.Execute(request);
+
+            var content = response.Content;
+
+            return content;
+        }
+    }
+}
