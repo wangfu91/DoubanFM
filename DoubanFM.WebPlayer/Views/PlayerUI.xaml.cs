2fda0a6bed756f615b6bf6ee8fcf7b9d7f42dbf3
WangFu <wangfu91@hotmail.com>
2014-12-17 00:53:51 +0800

    Finish Spectrum Analyzer...

---
+++ DoubanFM.WebPlayer/Views/PlayerUI.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DoubanFM.WebPlayer.Views
+{
+    /// <summary>
+    /// Interaction logic for PlayerUI.xaml
+    /// </summary>
+    public partial class PlayerUI : UserControl
+    {
+        public PlayerUI()
+        {
+            InitializeComponent();
+        }
+    }
+}
